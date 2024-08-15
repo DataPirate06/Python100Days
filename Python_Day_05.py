b93db2b67a047476db75999862891712c2cb155c
Mohit Tendolkar <121725299+DataPirate06@users.noreply.github.com>
2024-08-15 19:57:38 +0000

    Day 5 ended

---
+++ Python_Day_05.py
@@ -0,0 +1,15 @@
+#Comments, Escape Sequences & Print Statement
+
+#Insert new line character '\n' in the sentence
+print("Hey i am a good boy \nMy name is Mohit")
+'''
+Hey Mohit,please dont remove this line
+I am using it to understand multi line comments
+'''
+
+#Using double quotes sequence characters '\"' (Escape sequence characters)
+print("Hey i am a \"good boy\" \nMy name is Mohit")
+
+#joining using seperator
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Mohit")
