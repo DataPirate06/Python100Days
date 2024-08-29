e63c43b09126d1f9208258948835cc19ccdcd173
Mohit Tendolkar <121725299+DataPirate06@users.noreply.github.com>
2024-08-29 17:26:29 +0000

    day 23 commit

---
+++ Python_Day_23.py
@@ -0,0 +1,17 @@
+#List Methods
+l = [11, 45, 1, 2, 4, 6, 1, 1]
+print(l)
+# l.append(7)
+#l.sort()....ascending order
+# l.sort(reverse=True).... It will sort the list in descending order
+# l.reverse()
+# print(l.index(1))
+# print(l.count(1))
+# m = l.copy()....If m=l then it will change the value of l but by doing copy it will not change the value of l
+# m[0] = 0
+# l.insert(1, 899)
+m = [900, 1000, 1100]
+k = l + m  #If we want to add two list then we can use this method
+# print(k)
+# l.extend(m)
+print(l)
