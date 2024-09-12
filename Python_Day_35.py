ff2f5c378fbef09c2c3bd1e616a881c22807cef0
Mohit Tendolkar <121725299+DataPirate06@users.noreply.github.com>
2024-09-12 17:22:51 +0000

    commit day 35

---
+++ Python_Day_35.py
@@ -0,0 +1,19 @@
+#python-else clause
+#Python allows the else keyword to be used with the for and while loops too.
+#The program exits the loop only after the else block is executed.
+
+i = 0
+while i < 7:
+  print(i)
+  i = i + 1
+  # if i == 4:
+  #   break
+
+else:
+  print("Sorry no i")
+
+for x in range(5):
+  print("iteration no {} in for loop".format(x + 1))
+else:
+  print("else block in loop")
+print("Out of loop")
