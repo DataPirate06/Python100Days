ec0a2b8f4afc054c2145bfde1911d726b2c743e1
Mohit Tendolkar <tendolkarmohit@gmail.com>
2024-10-20 10:03:13 +0530

    Create Python_Day_44.py
    commit day 44

---
+++ Python_Day_44.py
@@ -0,0 +1,16 @@
+# import math
+
+# result = math.sqrt(9)
+# print(result)  # Output: 3.0
+
+# from math import sqrt
+
+# result = sqrt(9)
+# print(result)  # Output: 3.0
+
+import math as m
+
+result = m.sqrt(9)
+print(result)  # Output: 3.0
+
+print(m.pi)  # Output: 3.141592653589793
